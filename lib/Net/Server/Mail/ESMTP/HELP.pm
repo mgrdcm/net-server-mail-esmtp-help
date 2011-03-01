ad678e345091a2022ecbc6d81b39214e79a018f7
Dan Moore <dan@moore.cx>
2011-03-01 07:14:56 +0000

    Initial version of module skeleton.

---
+++ lib/Net/Server/Mail/ESMTP/HELP.pm
@@ -0,0 +1,110 @@
+package Net::Server::Mail::ESMTP::HELP;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Net::Server::Mail::ESMTP::HELP - The great new Net::Server::Mail::ESMTP::HELP!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Net::Server::Mail::ESMTP::HELP;
+
+    my $foo = Net::Server::Mail::ESMTP::HELP->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Dan Moore, C<< <dan at moore.cx> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-net-server-mail-esmtp-help at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Net-Server-Mail-ESMTP-HELP>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Net::Server::Mail::ESMTP::HELP
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Net-Server-Mail-ESMTP-HELP>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Net-Server-Mail-ESMTP-HELP>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Net-Server-Mail-ESMTP-HELP>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Net-Server-Mail-ESMTP-HELP/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Dan Moore.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Net::Server::Mail::ESMTP::HELP
